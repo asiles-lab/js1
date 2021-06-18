70df6966b2b9ba9f32ee0b8e30f9348b40130aef
asiles-lab <siles.htc@gmail.com>
2021-06-18 10:25:26 -0300

    first commit

---
+++ clase1.js
@@ -0,0 +1,16 @@
+// const miNombre = "Alejandro";
+// const apellido = "Siles";
+// const dni = 36165186;
+
+// const datos = miNombre +  " " + apellido + " " + "DNI" + " " + dni;
+
+// console.log(datos);
+
+
+// const edad = parseInt(prompt("Ingrese su edad"));
+// alert("Mi edad es" + " " + edad) ;
+
+const num1 = parseInt(prompt("Ingresa un numero"));
+const num2 = parseInt(prompt("Ingresa otro numero"));
+const num3 = (num1 + num2)
+alert ("La suma de los numeros es:   " + num3)
